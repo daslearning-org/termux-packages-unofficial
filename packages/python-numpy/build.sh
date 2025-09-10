06730e52af7d1a8fc18c60bfc8aaf111733d76e1
Somnath Das <somnath.das@daslearning.in>
2025-09-10 15:09:54 +0530

    using numpy 1.26.5 & new onnxruntime

--- packages/python-numpy/build.sh
+++ packages/python-numpy/build.sh
@@ -2,12 +2,17 @@ TERMUX_PKG_HOMEPAGE=https://numpy.org/
 TERMUX_PKG_DESCRIPTION="The fundamental package for scientific computing with Python"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.23.3"
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/numpy/numpy.git
-TERMUX_PKG_DEPENDS="libc++, python"
-TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_VERSION="1.26.5"
+TERMUX_PKG_SRCURL=git+https://github.com/numpy/numpy
+TERMUX_PKG_DEPENDS="libc++, libopenblas, python"
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
+TERMUX_PKG_PYTHON_COMMON_DEPS="wheel, 'Cython>=0.29.34,<3.1', 'meson-python>=0.15.0,<0.16.0', build"
+
+TERMUX_MESON_WHEEL_CROSSFILE="$TERMUX_PKG_TMPDIR/wheel-cross-file.txt"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--cross-file $TERMUX_MESON_WHEEL_CROSSFILE
+"
 
 TERMUX_PKG_RM_AFTER_INSTALL="
 bin/
@@ -17,73 +22,55 @@ termux_step_pre_configure() {
 	if $TERMUX_ON_DEVICE_BUILD; then
 		termux_error_exit "Package '$TERMUX_PKG_NAME' is not available for on-device builds."
 	fi
-	_PYTHON_VERSION=$(. $TERMUX_SCRIPTDIR/packages/python/build.sh; echo $_MAJOR_VERSION)
+
+	LDFLAGS+=" -lm"
 }
 
 termux_step_configure() {
-	termux_setup_python_crossenv
-	pushd $TERMUX_PYTHON_CROSSENV_SRCDIR
-	_CROSSENV_PREFIX=$TERMUX_PKG_BUILDDIR/python-crossenv-prefix
-	python${_PYTHON_VERSION} -m crossenv \
-		$TERMUX_PREFIX/bin/python${_PYTHON_VERSION} \
-		${_CROSSENV_PREFIX}
-	popd
-	. ${_CROSSENV_PREFIX}/bin/activate
+	termux_setup_meson
 
-	LDFLAGS+=" -lpython${_PYTHON_VERSION}"
+	cp -f $TERMUX_MESON_CROSSFILE $TERMUX_MESON_WHEEL_CROSSFILE
+	sed -i 's|^\(\[binaries\]\)$|\1\npython = '\'$(command -v python)\''|g' \
+		$TERMUX_MESON_WHEEL_CROSSFILE
+	sed -i 's|^\(\[properties\]\)$|\1\nnumpy-include-dir = '\'$PYTHON_SITE_PKG/numpy/core/include\''|g' \
+		$TERMUX_MESON_WHEEL_CROSSFILE
+
+	local _longdouble_format=""
+	if [ $TERMUX_ARCH_BITS = 32 ]; then
+		_longdouble_format="IEEE_DOUBLE_LE"
+	else
+		_longdouble_format="IEEE_QUAD_LE"
+	fi
+	sed -i 's|^\(\[properties\]\)$|\1\nlongdouble_format = '\'$_longdouble_format\''|g' \
+		$TERMUX_MESON_WHEEL_CROSSFILE
+
+	local _meson_buildtype="minsize"
+	local _meson_stripflag="--strip"
+	if [ "$TERMUX_DEBUG_BUILD" = "true" ]; then
+		_meson_buildtype="debug"
+		_meson_stripflag=
+	fi
+
+	local _custom_meson="build-python $TERMUX_PKG_SRCDIR/vendored-meson/meson/meson.py"
+	CC=gcc CXX=g++ CFLAGS= CXXFLAGS= CPPFLAGS= LDFLAGS= $_custom_meson \
+		$TERMUX_PKG_SRCDIR \
+		$TERMUX_PKG_BUILDDIR \
+		--cross-file $TERMUX_MESON_CROSSFILE \
+		--prefix $TERMUX_PREFIX \
+		--libdir lib \
+		--buildtype ${_meson_buildtype} \
+		${_meson_stripflag} \
+		$TERMUX_PKG_EXTRA_CONFIGURE_ARGS
 }
 
 termux_step_make() {
-	build-pip install pybind11 Cython pythran wheel
-	DEVICE_STIE=$TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages
-	MATHLIB="m" python setup.py install --force
+	pushd $TERMUX_PKG_SRCDIR
+	python -m build -w -n -x --config-setting builddir=$TERMUX_PKG_BUILDDIR .
+	popd
 }
 
 termux_step_make_install() {
-	export PYTHONPATH="$DEVICE_STIE"
-	MATHLIB="m" python setup.py install --force --prefix $TERMUX_PREFIX
-
-	pushd $DEVICE_STIE
-	_NUMPY_EGGDIR=
-	for f in numpy-${TERMUX_PKG_VERSION}-py${_PYTHON_VERSION}-linux-*.egg; do
-		if [ -d "$f" ]; then
-			_NUMPY_EGGDIR="$f"
-			break
-		fi
-	done
-	test -n "${_NUMPY_EGGDIR}"
-
-	# XXX: Fix the EXT_SUFFIX. More investigation is needed to find the underlying cause.
-	pushd "${_NUMPY_EGGDIR}"
-	local old_suffix=".cpython-310-$TERMUX_HOST_PLATFORM.so"
-	local new_suffix=".cpython-310.so"
-
-	find . \
-		-name '*'"$old_suffix" \
-		-exec sh -c '_f="{}"; mv -- "$_f" "${_f%'"$old_suffix"'}'"$new_suffix"'"' \;
-	popd
-
-	popd
-}
-
-termux_step_post_make_install() {
-	# Delete the easy-install related files, since we use postinst/prerm to handle it.
-	pushd $TERMUX_PREFIX
-	rm -rf lib/python${_PYTHON_VERSION}/site-packages/__pycache__
-	rm -rf lib/python${_PYTHON_VERSION}/site-packages/easy-install.pth
-	rm -rf lib/python${_PYTHON_VERSION}/site-packages/site.py
-	popd
-}
-
-termux_step_create_debscripts() {
-	cat <<- EOF > ./postinst
-	#!$TERMUX_PREFIX/bin/sh
-	echo "Installing numpy..."
-	echo "./${_NUMPY_EGGDIR}" >> $TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages/easy-install.pth
-	EOF
-
-	cat <<- EOF > ./prerm
-	#!$TERMUX_PREFIX/bin/sh
-	sed -i "/\.\/${_NUMPY_EGGDIR//./\\.}/d" $TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages/easy-install.pth
-	EOF
+	local _pyv="${TERMUX_PYTHON_VERSION/./}"
+	local _whl="numpy-$TERMUX_PKG_VERSION-cp$_pyv-cp$_pyv-linux_$TERMUX_ARCH.whl"
+	pip install --no-deps --prefix=$TERMUX_PREFIX --force-reinstall $TERMUX_PKG_SRCDIR/dist/$_whl
 }
